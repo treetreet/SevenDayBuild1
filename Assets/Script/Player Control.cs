d11054d7dd728c62ed10ddd4a033591b5c671c9f
treetreet <treetreet@kakao.com>
2025-01-27 19:04:13 +0900

    ADD CharactorAnimator

--- Assets/Script/Player Control.cs
+++ Assets/Script/Player Control.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    void Awake()
     {
-        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(moveX, moveY, 0);
+        transform.Translate(moveVector.normalized * Time.deltaTime * 5f);
+
+        if(moveX != 0 || moveY != 0)
+        {
+            animator.SetBool("1_Move", true);
+        }
+        else
+        {
+            animator.SetBool("1_Move", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            animator.SetTrigger("2_Attack");
+        }
     }
 }
